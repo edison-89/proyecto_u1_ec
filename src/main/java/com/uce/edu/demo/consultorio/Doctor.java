cdb880eeccbd6ac019d4cac67d0b3d123cf5f895
Edison Cayambe <edisonc_89@hotmail.com>
2022-05-27 20:36:01 -0500

    trecer

--- src/main/java/com/uce/edu/demo/consultorio/Doctor.java
+++ src/main/java/com/uce/edu/demo/consultorio/Doctor.java
@@ -4,6 +4,15 @@ public class Doctor {
 	private String nombre;
 	private String apellido;
 	private String edad;
+	private String ciudad1;
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
 
 	public String getEdad() {
 		return edad;
