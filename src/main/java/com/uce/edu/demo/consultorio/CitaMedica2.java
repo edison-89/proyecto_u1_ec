4026dffd0eb12bb479be4ee2f6b8a6350b6081f4
Edison Cayambe <edisonc_89@hotmail.com>
2022-05-30 20:04:02 -0500

    Taller de modelos IoC y DI

--- src/main/java/com/uce/edu/demo/consultorio/CitaMedica2.java
+++ src/main/java/com/uce/edu/demo/consultorio/CitaMedica2.java
@@ -2,21 +2,17 @@ package com.uce.edu.demo.consultorio;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CitaMedica {
+public class CitaMedica2 {
 
 	private LocalDateTime fechaCita;
 
-	private Doctor doctor;
-	private Paciente paciente;
-
-	//2) DI por constructor
-	public CitaMedica(Doctor doctor, Paciente paciente) {
-		this.doctor = doctor;
-		this.paciente = paciente;
-	}
+	//DI por metodos SET
+	private Doctor doctor;// 3) a su metodo SET le pongo @Autowired
+	private Paciente paciente;// a su metodo SET le pongo @Autowired
 
 	public String agendar(LocalDateTime fechaCita, String nombre, String apellido, int edad, String ciudad,
 			String nombrePaciente, int edadPaciente) {
@@ -33,7 +29,7 @@ public class CitaMedica {
 		this.fechaCita = fechaCita;
 
 		// Se inserta la cita en la base de datos
-		return "Cita agendada";
+		return "Cita agendada 2";
 	}
 
 	// SET Y GET
@@ -50,6 +46,7 @@ public class CitaMedica {
 		return doctor;
 	}
 
+	@Autowired
 	public void setDoctor(Doctor doctor) {
 		this.doctor = doctor;
 	}
@@ -58,6 +55,7 @@ public class CitaMedica {
 		return paciente;
 	}
 
+	@Autowired
 	public void setPaciente(Paciente paciente) {
 		this.paciente = paciente;
 	}
